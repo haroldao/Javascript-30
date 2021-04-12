8057ade0378955fa97f6bad823c4e5d205f6b189
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-12 16:56:32 +0200

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,4 +1,8 @@
 window.addEventListener('keydown', (e)=>{
-    // console.log(e.keyCode)
-    const audio = document.querySelector('audio');
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stop the function
+    console.log(audio)
+    audio.currentTime=0; //Rewind to the start
+    audio.play();
+
 })
