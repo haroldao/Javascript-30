a6a54d720a900247e501e76d83c6bfaf8d1bd49b
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-12 18:03:12 +0200

    Done 🔥🔥🔥 ... Now styling

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,7 +1,8 @@
-window.addEventListener('keydown', (e)=>{
+window.addEventListener('keydown', playSound)
+
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
 
     if(!audio) return; // stop the function
     // console.log("audio", audio);
@@ -11,5 +12,11 @@ window.addEventListener('keydown', (e)=>{
     // console.log("key", key);
     key.classList.add("playing");
 
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach( key => key.addEventListener("transitionend", removeTransition));
 
-})
+function removeTransition (event){
+//    console.log(event);
+    this.classList.remove("playing");
+}
