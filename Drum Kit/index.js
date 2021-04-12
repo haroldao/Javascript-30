5237ae32e3eaae36c612155e14dc624b0453e6bb
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-12 17:36:52 +0200

    WIP

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,8 +1,15 @@
 window.addEventListener('keydown', (e)=>{
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+
     if(!audio) return; // stop the function
-    console.log(audio)
+    // console.log("audio", audio);
     audio.currentTime=0; //Rewind to the start
     audio.play();
+
+    // console.log("key", key);
+    key.classList.add("playing");
+
 
 })
