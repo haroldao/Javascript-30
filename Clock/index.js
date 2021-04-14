03374883ec58ee020f24e83ebd6a8825d4e6200f
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-14 20:30:53 +0200

    Rotation works

--- Clock/index.js
+++ Clock/index.js
@@ -1,7 +1,11 @@
+const secondHand = document.querySelector(".second-hand");
+
 setInterval(function sayHi(){
     const now = new Date();
     const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60)*360);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     const minutes = now.getMinutes();
-    console.log(seconds)
+    console.log(seconds);
 },1000)
 
