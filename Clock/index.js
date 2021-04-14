695d466799c7e831aabf2d2a570bc9d741c6dedf
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-14 20:46:10 +0200

    Done

--- Clock/index.js
+++ Clock/index.js
@@ -1,11 +1,35 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 setInterval(function sayHi(){
     const now = new Date();
+
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60)*360);
+    const secondsDegrees = ((seconds / 60)*360) + 90; // 90  == (default degree)
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
     const minutes = now.getMinutes();
-    console.log(seconds);
+    console.log(minutes)
+    const minutesDegrees = ((minutes / 60)*360) + 90;
+    minuteHand.style.transform= `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    console.log(hours)
+    const hoursDegrees = ((hours / 12)*360) + 90;
+    hourHand.style.transform= `rotate(${hoursDegrees}deg)`;
+
+    if(seconds==0){
+        secondHand.style.transitionDuration = '0s';
+        minuteHand.style.transitionDuration = '0s';
+        hourHand.style.transitionDuration = '0s';
+    } 
+    else {
+        secondHand.style.transitionDuration = '0.05s';
+        minuteHand.style.transitionDuration = '0.05s';
+        hourHand.style.transitionDuration = '0.05s';
+    }
+
+    // console.log(seconds);
 },1000)
 
